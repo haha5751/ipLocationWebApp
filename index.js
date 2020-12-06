e333bf3dde9b5d094d9fc410b67e0868afd0b7f3
Harris Hafeez <harrishafeez@Harriss-MBP.lan>
2020-12-05 17:56:32 -0700

    css error continuing

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.set('view engine', 'ejs');
 app.use(express.static("public"));
+app.set('view engine', 'ejs');
+
 // const server = https.createServer(function(req, res) {
 
 // })
