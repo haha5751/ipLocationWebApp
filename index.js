54c2c4a084780717f2d84f8f5cad195f0c0e144b
Harris Hafeez <harrishafeez@Harriss-MBP.lan>
2020-12-05 18:18:58 -0700

    reupdated file structure

--- index.js
+++ index.js
@@ -5,12 +5,10 @@ const port = 3000;
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 
-// const server = https.createServer(function(req, res) {
-
-// })
 app.get("/", (req, res) => {
     res.render('pages/home');
 })
+
 app.listen(port, function(error) {
     if(error) {
         console.log("Error", error)
