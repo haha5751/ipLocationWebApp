0f1f20da4cda0cd5d1fe77001d4e6ffe0dbe2854
Harris Hafeez <harrishafeez@Harriss-MBP.lan>
2020-12-09 14:31:50 -0700

    massive changes

--- index.js
+++ index.js
@@ -1,48 +1,77 @@
 // initializtion
-const express = require('express');
+const express = require('express'); 
 const app = express();
 const port = 3000;
 const mongoose = require('mongoose');
-IPLocation = require("./models/IPModels")
-const assert = require('assert');
-const url = "mongodb+srv://haha5751:hh431809@cluster0.dayqc.mongodb.net/3308DB?retryWrites=true&w=majority";
-
-mongoose.connect(url, {
-  useNewUrlParser: true
+const dbUrl = "mongodb+srv://haha5751:hh431809@cluster0.dayqc.mongodb.net/3308DB?retryWrites=true&w=majority";
+IPLocations = require("./models/IPModels");
+mongoose.connect(dbUrl, {
+    'useNewUrlParser': true
 });
+// connect to db
 mongoose.set('useUnifiedTopology', true);
+
+// convert ip to long
+// credit: https://gist.github.com/jppommet/5708697
+function ip2int(ip) {
+    return ip.split('.').reduce(function(ipInt, octet) { return (ipInt<<8) + parseInt(octet, 10)}, 0) >>> 0;
+}
+
+// check if ip is valid
+// credit : https://www.w3resource.com/javascript/form/ip-address-validation.php
+function isProperIP(ip) {  
+    if (/^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(ip)) {  
+      return (true)  
+    }   
+    return (false)
+  }  
 
 // stylesheets, scripts and img
 app.use(express.static("public"));
 
-// use ejs
+// ejs
 app.set('view engine', 'ejs');
 
-// get requests
+// app get requests
+
+// home
 app.get("/", (req, res) => {
-    IPLocation.find({}, (err, IPLocations) => {
-        if(err) {
-            console.log("Error Line 27, index.j s", err);
-        } else {
-            res.render('pages/home', {
-                data: IPLocations,
-                title: "home"
-            });
-        }
-    });
+    res.render('pages/home', {
+        title: "Home"
+    }); 
 });
+// getLocation
 app.get("/getLocation", (req, res) => {
-    IPLocation.find({}, (err, IPLocations) => {
-        if(err) {
-            console.log("Error Line 40, index.j s", err);
-        } else {
-            res.render('pages/getLocation', {
-                data: IPLocations,
-                title: "getLocation"
-            });
-        }
+    res.render('pages/getLocation', {
+        title: "getLocation",
+        result: "0"
     });
 });
+// getLocation user submits IP 
+app.get("/getLocation/submit", (req, res) => {
+    var ip_address = req.query.getLocation_ip_address;
+    var ip_location = "0";
+    if(isProperIP(ip_address)) {
+        ip_address = ip2int(ip_address);
+        ip_location = {ip_end:{$gte:ip_address}}; 
+        IPLocations.findOne(ip_location, function(err, results) {
+            if(err) {
+                console.log("Error, line 65 index.js", err);
+            } else {
+                res.render('pages/getLocation', {
+                    title: "Get Location",
+                    result: results
+                });
+            }
+        });
+    } else {
+        ip_location = "error";
+        res.render('pages/getLocation', {
+            title: "getLocation",
+            result: ip_location 
+        });      
+    }
+});   
 
 // localhost
 app.listen(port, function(error) {
@@ -52,24 +81,3 @@ app.listen(port, function(error) {
         console.log("Server is listening " + port)
     }
 })
-
-
-    // MongoClient.connect(url, function (err, client) {
-    //     if(err) {
-    //         console.log("Error, MongoClient ", err);
-    //     } else {
-    //         const dbo = client.db("3308DB");
-    //         const ipLocation = dbo.collection("ipLocation");
-    //         const query = {ip_start: "16777216"};
-    //         ipLocation.findOne(query, function (err, ipLocations) {
-    //             if(err) {
-    //                 console.log("Error, ipLocation.find ", err);
-    //             } else {
-    //                 console.log("ipLocations ",ipLocations);
-    //                 res.render('pages/getLocation', { data: ipLocations});
-    //             }
-    //         });
-    //     }
-    // });
-
-// mongoimport -u mongoAdmin -p changeMe --authenticationDatabase admin --drop --db ip2location --collection db24 --type csv --file "/Users/harrishafeez/Downloads/IP2LOCATION-LITE/IP2LOCATION-LITE-DB1.CSV" --fields ip_from,ip_to,country_code,country_name
--- models/IPModels.js
+++ models/IPModels.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
 const IPModelschema = new mongoose.Schema({
-    ip_start: String,
-    ip_end: String,
+    ip_start: Number,
+    ip_end: Number,
     country_code: String,
     country_name: String
 });
 
-module.exports = mongoose.model("IPLocation", IPModelschema);
+module.exports = mongoose.model("IPLocations", IPModelschema, "ipLocation");
